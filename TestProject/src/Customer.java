c3d83c6942b748fd65bbc48c253ac94ab03f7078
rasheee03 <rasheeeu@gmail.com>
2019-11-02 17:48:48 +0530

    df

---
+++ TestProject/src/Customer.java
@@ -0,0 +1,19 @@
+
+public class Customer {
+int CustId;
+int id;
+String CustName;
+public int getCustId() {
+	return CustId;
+}
+public void setCustId(int custId) {
+	CustId = custId;
+}
+public String getCustName() {
+	return CustName;
+}
+public void setCustName(String custName) {
+	CustName = custName;
+}
+
+}
